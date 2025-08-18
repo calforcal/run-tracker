90fd9fce055ef259dc25174d1f7856145e7181d2
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2025-08-17 20:58:54 -0600

    Add ActivityRow component for list view of activites

---
+++ src/components/ActivityRow/ActivityRow.tsx
@@ -0,0 +1,20 @@
+import type { Activity } from "../../types/athlete";
+import { calculateElapsedTime } from "../../utils/DateTime";
+import styles from "./ActivityRow.module.css";
+
+interface ActivityProps {
+  activity: Activity;
+}
+
+export const ActivityRow: React.FC<ActivityProps> = ({ activity }) => {
+  return (
+    <div className={styles.activityContainer}>
+      <h3>{activity.name}</h3>
+      <div className={styles.detailsRow}>
+        <div>{activity.sportType}</div>
+        <div>{(activity.distance / 100).toFixed(2)} KM</div>
+        <div>{calculateElapsedTime(activity.movingTime)} Minutes</div>
+      </div>
+    </div>
+  );
+};
